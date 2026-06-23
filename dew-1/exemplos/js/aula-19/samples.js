7d1b9232d2c027ebbfa44a253b8ace6c31f19b59
Sabrina <sabrinabm94@gmail.com>
2026-06-23 15:35:56 -0300

    melhorias de comentários e código na aula 18

--- dew-1/exemplos/js/aula-19/samples.js
+++ dew-1/exemplos/js/aula-19/samples.js
@@ -26,23 +26,53 @@ console.log(description);
 // _____________________________________________________________________________________________________________________
 
 // MANIPULAÇÃO DE CONTEÚDO
-title.textContent = "JavaScript Moderno";
+// Seleção e alteração do conteúdo de um elemento
+const newListElement = document.createElement("li");
+
+//TextContent altera o conteúdo de um elemento
+newListElement.textContent = "Sou um novo item";
+console.log(newListElement);
 
 // _____________________________________________________________________________________________________________________
 
-// EVENTOS
-// Permitem executar ações quando algo acontece na página
-const button = document.querySelector("#btn");
+// ADIÇÃO DE ELEMENTOS
+// Seleção e adição de um novo elemento filho
+const list = document.querySelector("ul");
 
-// Evento de clique
-button.addEventListener("click", function () {
-  console.log("Botão clicado");
-});
+// AppendChild adiciona um elemento filho no final da lista
+list.appendChild(newListElement);
+console.log(list);
+
+// Append adiciona um elemento filho no final da lista
+list.append("Fim da lista ");
+console.log(list);
+
+const anotherNewListElement = document.createElement("li");
+anotherNewListElement.textContent = "Git";
+
+// Prepend adiciona um elemento filho no início da lista
+list.prepend(anotherNewListElement);
+console.log(list);
+
+const otherNewListElement = document.createElement("li");
+otherNewListElement.textContent = "GitHub";
+
+// InsertBefore adiciona um elemento filho antes de outro elemento filho
+list.insertBefore(otherNewListElement, anotherNewListElement);
+console.log(list);
 
 // _____________________________________________________________________________________________________________________
 
-// EVENTO UTILIZANDO FUNÇÃO
-function showMessage() {
-  console.log("Evento executado");
-}
-button.addEventListener("click", showMessage);
+// REMOÇÃO DE ELEMENTOS
+const secondItemOfList = document.querySelectorAll("li")[1];
+
+// Remove remove um elemento da lista
+secondItemOfList.remove();
+console.log(list);
+
+// RemoveChild remove um elemento filho da lista
+const itemToRemove = list.children[0];
+list.removeChild(itemToRemove);
+console.log(list);
+
+// _____________________________________________________________________________________________________________________
