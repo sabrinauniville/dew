584f978764a21ca28c76f281392930c68a102eb5
Sabrina <sabrinabm94@gmail.com>
2026-07-07 16:53:41 -0300

    implementação correta de roteamento e padrão standalone

--- dew-1/exemplos/js/aula-22/projeto/src/app/app.routes.ts
+++ dew-1/exemplos/js/aula-22/projeto/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePage },
+  { path: 'home-page', redirectTo: 'home' },
+  { path: '**', redirectTo: 'home' },
+];
