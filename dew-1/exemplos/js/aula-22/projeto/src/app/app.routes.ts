ebfabe1fd73cfeec3192eddc3b09a2f19e6cc198
Sabrina <sabrinabm94@gmail.com>
2026-07-09 11:59:47 -0300

    atualização de comentários

--- dew-1/exemplos/js/aula-22/projeto/src/app/app.routes.ts
+++ dew-1/exemplos/js/aula-22/projeto/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
 
+/* Cada página deverá ter sua própria rota de acesso */
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePage },
-  { path: 'home-page', redirectTo: 'home' },
-  { path: '**', redirectTo: 'home' },
+  { path: 'home-page', redirectTo: 'home' } /* Rota para a página inicial */,
+  { path: '**', redirectTo: 'home' } /* Rota para páginas não encontradas */,
 ];
